094e69b176845369a9a9a1f0c580935ae931a7c2
[vinald] <okiror1vinald@gmail.com>
2026-08-14 21:55:12 +0300

    Scaffold cflassist OpenMRS module

---
+++ src/main/java/org/openmrs/module/cflassist/CflAssistActivator.java
@@ -0,0 +1,11 @@
+package org.openmrs.module.cflassist;
+
+import org.openmrs.module.BaseModuleActivator;
+
+/**
+ * No-op activator. The module's only behavior is the declaratively-registered
+ * {@link org.openmrs.module.cflassist.filter.WidgetInjectionFilter} in config.xml - there is no
+ * start/stop logic to run.
+ */
+public class CflAssistActivator extends BaseModuleActivator {
+}
