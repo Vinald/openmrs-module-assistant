5f6a6cb7690ca21f116eb3d00812c5e0d5d70555
Okiror Samuel Vinald <94779366+Vinald@users.noreply.github.com>
2026-08-20 20:00:18 +0300

    Add LICENSE and document authorship

--- src/main/java/org/openmrs/module/cflassist/filter/WidgetInjectionFilter.java
+++ src/main/java/org/openmrs/module/cflassist/filter/WidgetInjectionFilter.java
@@ -1,3 +1,11 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) Okiror Samuel Vinald. All Rights Reserved.
+ */
 package org.openmrs.module.cflassist.filter;
 
 import java.io.ByteArrayOutputStream;
@@ -76,6 +84,18 @@ public class WidgetInjectionFilter implements Filter {
 				chain.doFilter(request, response);
 				return;
 			}
+		}
+
+		// OWAs (React/Angular/etc bundles under /owa/) are pre-built static asset trees, not
+		// per-request dynamic HTML - buffering their index.html here proved unreliable under
+		// real browser concurrent load (intermittent malformed/503 responses that made Chrome
+		// silently abandon navigation, breaking every OWA-routed tile - Register Patient, Find
+		// Patient, etc). The widget script is baked directly into each OWA's packaged
+		// index.html instead (see openmrs-distro-cfl's web/owa/cfl.owa), so this filter doesn't
+		// need to touch OWA responses at all.
+		if (lowerUri.contains("/owa/")) {
+			chain.doFilter(request, response);
+			return;
 		}
 
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
